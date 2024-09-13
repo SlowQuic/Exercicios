7e86696a33b7d5698ff1648053637dd1b670b41c
SlowQuick <132625066+SlowQuic@users.noreply.github.com>
2024-09-12 23:12:50 -0300

    EXERCICIO CONSUMO
    Digite a distancia percorrida, o tanto de combustivel que foi consumida, e achar a media de combustivel gasto com 3 casas decimais

---
+++ consumo.py
@@ -0,0 +1,8 @@
+print("Distancia percorrida:")
+distancia = float(input())
+print("Combustível gasto:")
+combustivel = float(input())
+
+media = distancia / combustivel
+
+print(f"MEDIA: {media:.5}")
