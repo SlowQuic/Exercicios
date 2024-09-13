9b068675cbc9fdc210f021d65b9027b1f532aa18
SlowQuick <132625066+SlowQuic@users.noreply.github.com>
2024-09-12 23:23:16 -0300

    EXERCICIO MEDIDAS
    Bem básico tbm, calcular a medida da área de um quadrado, trianngulo e trapezio após digitar o valor de A, B e C

---
+++ medidas.py
@@ -0,0 +1,26 @@
+print("Digite a medida A:")
+A = float(input())
+print("Digite a medida B:")
+B = float(input())
+print("Digite a medida C:")
+C = float(input())
+
+AreaQuadrado = A ** 2
+AreaTriangulo = (A * B) / 2
+AreaTrapezio = (A + B) * C / 2 
+
+print(f"Area do Quadrado: {AreaQuadrado:.6}")
+print(f"Area do Triangulo: {AreaTriangulo:.6}")
+print(f"Area do Trapezio:  {AreaTrapezio:.6}")
+
+
+
+#a) a área do quadrado que tem lado A 
+#b) a área do triângulo retângulo que base A e altura B 
+#c) a área do trapézio que tem bases A e B, e altura C 
+#Digite a medida A: 4.0
+#Digite a medida B: 3.5
+#Digite a medida C: 5.2
+#AREA DO QUADRADO = 16.0000 
+#AREA DO TRIANGULO = 7.0000 
+#AREA DO TRAPEZIO = 19.5000
