b48fd45de48a35ef094e55efc7cb683b09300747
SlowQuick <132625066+SlowQuic@users.noreply.github.com>
2024-09-12 23:06:33 -0300

    EXERCICIO PAGAMENTO
    Básico, colocar o nome da pessoa, quantas horas ela trabalhou, o valor pago por hora, e no final mostrando o nome da pessoa e o total a ser pago

---
+++ pagamento.py
@@ -0,0 +1,17 @@
+print("Nome:")
+nome = input()
+print("Valor por hora:")
+valorhora = float(input())
+print("Horas trabalhas:")
+horas = float(input())
+
+pagamento = valorhora * horas
+
+print(f"O pagamento para", nome,"deve ser", pagamento)
+
+
+
+#Nome: Joao Silva
+#Valor por hora: 50.00
+#Horas trabalhadas: 60
+#O pagamento para Joao Silva deve ser 3000.00
