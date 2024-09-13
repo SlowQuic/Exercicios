53379c4a182cab02b27e558cfb7026bdcd65e592
SlowQuick <132625066+SlowQuic@users.noreply.github.com>
2024-09-13 11:42:19 -0300

    EXERCICIO GLICOSE
    Bem simples, foi bom pra fixar mais os comandos e variação do if, se o nivel de glicose for menor ou igual 100 é normal, se for maior que 100 e menor ou igual a 140 é elevado, se for maior que 140 é elevado

---
+++ glicose.py
@@ -0,0 +1,20 @@
+print("Digite a medida da glicose:")
+x = float(input())
+
+if x <= 100:
+    print("Classificação: Normal")
+elif x > 100 and x <= 140:
+    print("Classificação: Elevado")
+else:
+    print("Classificação: Diabetes")
+        
+
+
+
+#Normal Até 100 mg/dl 
+#Elevado 
+#Maior que 100 até 140 mg/dl
+#Diabetes Maior de 140 mg/dl 
+
+#Digite a medida da glicose: 90.0
+#Classificacao: normal
