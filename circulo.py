2cee6f3f630da380d97c71bf4adc00f4681a2064
SlowQuick <132625066+SlowQuic@users.noreply.github.com>
2024-09-12 22:57:17 -0300

    EXERCICIO CIRCULO
    Um pouco mais dificil, mas nada demais, só precisei usar o "import math", para usar o math.pi para calcular a área, mas ainda foi fácil

---
+++ circulo.py
@@ -0,0 +1,11 @@
+import math
+print("Digite o valor do raio do circulo:")
+raio_str = float(input())
+raio = raio_str
+
+area = math.pi * (raio ** 2)
+
+print(f"AREA: {area:.3f}")
+
+#Digite o valor do raio do circulo: 2.0
+#AREA = 12.566 
