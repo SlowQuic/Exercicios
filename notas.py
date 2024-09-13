4951df76f0631bd3b73334cc80fb0d1d2d558b17
SlowQuick <132625066+SlowQuic@users.noreply.github.com>
2024-09-13 09:45:02 -0300

    EXERCICIO NOTAS
    Bem simples, mas para aprender também sobre o "if" em python, coloque dois valores, se a soma dos dois for menor do 60, exibir mensagem, bem simples

---
+++ notas.py
@@ -0,0 +1,9 @@
+print("Digite a primeira nota:")
+X = float(input())
+print("Digite a segunda nota:")
+Y = float(input())
+Soma = X + Y
+
+print(f"Soma: {Soma:.2f}")
+if Soma < 60.0:
+    print("REPROVADO")   
