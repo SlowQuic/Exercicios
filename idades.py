5d74caba3f17758ce7a71523a4be24217a13fdbf
SlowQuick <132625066+SlowQuic@users.noreply.github.com>
2024-09-12 22:38:07 -0300

    EXERCICIO IDADES
    Neste, preciso criar um código para digitar o nome de 2 pessoas, depois suas idades, e um textinho final, falando seus nomes, e a media de suas idades

---
+++ idades.py
@@ -0,0 +1,22 @@
+print("Dados da primeria pessoa:")
+nome1 = input()
+idade1 = int(input())
+
+print("Dados da segunda pessoa:")
+nome2 = input()
+idade2 = int(input())
+
+media = (idade1 + idade2) / 2
+
+print("A idade média de", nome1,"e", nome2, "é", media, "anos.")
+
+
+
+
+#Dados da primeira pessoa: 
+#Nome: Maria Silva
+#Idade: 19
+#Dados da segunda pessoa: 
+#Nome: Joao Melo
+#Idade 20
+#A idade média de Maria Silva e Joao Melo é de 19.5 anos
